eeaecf87c3077d2660ba351ad9447c04121b3bf6
Luan Araujo <luanaraujo9977@gmail.com>
2021-10-21 22:34:36 -0300

    segunda implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,36 +20,21 @@ public class Program {
 		Double valorPorHora = cs.calcValorPorHora();
 		
 		if (valorPorHora <= 900) {
-			reajuste = 1;
+			System.out.println("Salario liquido: R$ " + valorPorHora);
 		}
 		else if (valorPorHora > 900 && valorPorHora <= 1500) {
-			reajuste = 2;
+			reajuste = 5;
+			cs.printSalary(reajuste);
 		}
 		else if (valorPorHora > 1500 && valorPorHora <= 2500){
-			reajuste = 3;
+			reajuste = 10;
+			cs.printSalary(reajuste);
 		}
 		else {
-			reajuste = 4;
-		}
-		
-		switch (reajuste) {
-		case 1:
-			System.out.println("Salario liquido: R$ " + valorPorHora);
-			break;
-		case 2:
-			cs.printSalary(reajuste, 5);
-			break;
-		case 3:
-			cs.printSalary(reajuste, 10);
-			break;
-		case 4:
-			cs.printSalary(reajuste, 20);
-			break;
-		default: 
-			System.out.println("Valor invalido");
+			reajuste = 20;
+			cs.printSalary(reajuste);
 		}
 		
 		sc.close();
 	}
-
 }
--- src/entities/CalculoSalario.java
+++ src/entities/CalculoSalario.java
@@ -27,60 +27,27 @@ public class CalculoSalario {
 	}
 	
 	public Double calcIR(int n){
-		if (n == 1) {
-			return 0.0;
-		}
-		if (n == 2) {
 			return (calcValorPorHora()*5)/100;
-		}
-		if (n == 3) {
-			return (calcValorPorHora()*10)/100;
-		}
-		else {
-			return (calcValorPorHora()*20)/100;
-		}
 	}
 	
 	public Double calcDescontoTotal(int n) {
-		if (n == 1) {
-			return 0.0;
-		}
-		if (n == 2) {
-			return ((calcValorPorHora()*5)/100) + ((calcValorPorHora()*10)/100);
-		}
-		if (n == 3) {
-			return ((calcValorPorHora()*10)/100) + ((calcValorPorHora()*10)/100);
-		}
-		else {
-			return ((calcValorPorHora()*20)/100) + ((calcValorPorHora()*10)/100);
-		}
+			return ((calcValorPorHora()*n)/100) + ((calcValorPorHora()*10)/100);
 	}
 	
 	public Double calcSalarioLiquido(int n) {
-		if (n == 1) {
-			return 0.0;
-		}
-		if (n == 2) {
-			return (valorHora*hora - ((calcValorPorHora()*5)/100 + (calcValorPorHora()*10)/100));
-		}
-		if (n == 3) {
-			return (valorHora*hora - ((calcValorPorHora()*10)/100 + (calcValorPorHora()*10)/100));
-		}
-		else {
-			return (valorHora*hora - ((calcValorPorHora()*20)/100 + (calcValorPorHora()*10)/100));
-		}
+			return (valorHora*hora - ((calcValorPorHora()*n)/100 + (calcValorPorHora()*10)/100));
 	}
 	
 	public Double calcValorPorHora() {
 		return valorHora * hora;
 	}
 	
-	public void printSalary(int n, int x) {	
-		System.out.println("Salario bruto: (" + valorHora + "*" + hora + ")" + ": R$ " + calcValorPorHora());
-		System.out.println("(-) IR (" + x + "%): R$ " + calcIR(n));
-		System.out.println("(-) INSS (10%): R$ " + (calcValorPorHora()*10)/100);
-		System.out.println("FGTS (11%): R$ " + (calcValorPorHora()*11)/100);
-		System.out.println("Total de desconto: R$ " + calcDescontoTotal(n));
-		System.out.println("Salario liquido: R$ " + calcSalarioLiquido(n));
+	public void printSalary(int n) {	
+		System.out.println("Salario bruto: (" + valorHora + "*" + hora + ")" + ": R$ " + String.format("%.2f", calcValorPorHora()));
+		System.out.println("(-) IR (" + n + "%): R$ " + String.format("%.2f", calcIR(n)));
+		System.out.println("(-) INSS (10%): R$ " + String.format("%.2f",(calcValorPorHora()*10)/100));
+		System.out.println("FGTS (11%): R$ " + String.format("%.2f", (calcValorPorHora()*11)/100));
+		System.out.println("Total de desconto: R$ " + String.format("%.2f", calcDescontoTotal(n)));
+		System.out.println("Salario liquido: R$ " + String.format("%.2f", calcSalarioLiquido(n)));
 	}
 }
